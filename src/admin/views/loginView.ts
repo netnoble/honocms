d5d232f2fa8a04cfac1fa962679e1c0c2ed84147
cooperding <cooperding@dingtalk.com>
2025-02-19 21:55:23 +0800

    seo

--- src/admin/views/loginView.ts
+++ src/admin/views/loginView.ts
@@ -36,7 +36,7 @@ export const loginView = (c:Context) =>
                                                 <div class="layui-input-prefix">
                                                     <i class="layui-icon layui-icon-password"></i>
                                                 </div>
-                                                <input type="password" name="password" value="123456"
+                                                <input type="password" name="password" value=""
                                                        lay-verify="required" placeholder="密   码"
                                                        lay-reqtext="请填写密码" autocomplete="off" class="layui-input"
                                                        lay-affix="eye">
@@ -77,9 +77,9 @@ export const loginView = (c:Context) =>
                 form.on('submit(demo-login)', function (data) {
                     let field = data.field; // 获取表单字段值
                     // 显示填写结果，仅作演示用
-                    layer.alert(JSON.stringify(field), {
-                        title: '当前填写的字段值'
-                    });
+                    // layer.alert(JSON.stringify(field), {
+                    //     title: '当前填写的字段值'
+                    // });
                     // 此处可执行 Ajax 等操作
                     // 发送Ajax请求
                     $.ajax({
@@ -88,7 +88,6 @@ export const loginView = (c:Context) =>
                         contentType: "application/json;charset=utf-8",
                         data: JSON.stringify(field), // 将表单数据转换为JSON字符串
                         success: function (res) {
-                            console.log('res.data.token', res.data.token)
                             // 假设接口返回的状态码是200表示成功
                             if (res.code === 200) {
                                 // 存储token到本地缓存
