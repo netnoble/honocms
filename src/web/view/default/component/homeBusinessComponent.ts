678cb3f9314d9892639f346735d9683d148c08e0
cooperding <cooperding@dingtalk.com>
2025-02-22 13:38:06 +0800

    template

--- src/web/view/default/component/homeBusinessComponent.ts
+++ src/web/view/default/component/homeBusinessComponent.ts
@@ -2,99 +2,104 @@ import {html,raw} from "hono/html";
 import {Context} from "hono";
 import {getAdDetail} from "@/web/logic/adLogic";
 
-export const homeAdvantageComponent = async (c: Context ) => {
+export const homeBusinessComponent = async (c: Context ) => {
 
     const adBanner = await getAdDetail(c,'home-banner');
     console.log('adBanner',adBanner);
     return html`
         <style>
-            .honocms-home-advantage {
-                text-align: center;
+            .honocms-home-business {
+                background: #fbfdff;
             }
 
-            .honocms-home-advantage .box-header {
-                margin-top: 70px;
+            .honocms-home-business .box-header {
                 margin-bottom: 30px;
+                text-align: center;
+                padding-top: 70px;
             }
 
-            .honocms-home-advantage .box-header .title {
+            .honocms-home-business .box-header .title {
                 font-size: 38px;
                 color: #2b3346;
                 line-height: 1.5;
                 font-weight: 600;
             }
-            .honocms-home-advantage .box-header .sub-title {
+            .honocms-home-business .box-header .sub-title {
                 font-size: 15px;
                 color: #5a6071;
                 margin-top: 10px;
             }
-            .honocms-home-advantage .box-content {
+            .honocms-home-business .box-content {
                 margin: 40px 0;
             }
-            .honocms-home-advantage .box-content .image{
-                width: 52px;
-                height: 50px;
+            .honocms-home-business .box-content .image{
+                height: 160px;
                 text-align: center;
                 display: inline-block;
             }
-            .honocms-home-advantage .box-content .title{
+            .honocms-home-business .box-content .image .img{
+                width: 100%;
+                height: 160px;
+                
+            }
+            .honocms-home-business .box-content .title{
                 font-weight: bold;
                 font-size: 16px;
                 color: #43464d;
                 text-align: center;
                 margin-top: 20px;
             }
-            .honocms-home-advantage .box-content .sub-title{
+            .honocms-home-business .box-content .sub-title{
                 font-size: 15px;
                 color: #6f7480;
                 line-height: 25px;
                 text-align: center;
                 margin-top: 15px;
             }
-            .honocms-home-advantage .box-content .block{
+            .honocms-home-business .box-content .block{
                 margin: 20px 0;
-                padding: 30px 30px;
+                padding: 26px 20px;
                 border: 1px solid #ebebeb;
                 border-radius: 6px;
                 box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1), 0 6px 20px rgba(0, 0, 0, 0.1);
             }
         </style>
-        <div class="honocms-home-advantage">
+        <div class="honocms-home-business">
             <div class="layui-container ">
                 <!-- 主要内容 -->
                 
                 <div class="box-header">
-                    <div class="title">HonoCMS 核心优势</div>
-                    <div class="sub-title">做企业官网当然要选一套好用的CMS</div>
+                    <div class="title">探索HonoCMS商业化服务：开启您的数字成功之旅</div>
+                    <div class="sub-title">HonoCMS为您提供一站式解决方案，通过我们的专业商业化服务，帮助您打造个性化、高效的网站与应用平台。</div>
                 </div>
                 <div class="box-content">
                     <div class="layui-row layui-col-space30">
                         <!-- 左侧图片 -->
                         <div class="layui-col-md4">
                             <div class="block">
-                                <div class="image"><img src="https://www.huocms.com/static/home/images/new_index/Advantage_ico01.png"/></div>
-                                <div class="title">开源、免费、可商用</div>
+                                <div class="image"><img class="img" src="https://www.huocms.com/static/home/images/new_index/new01.jpg"/></div>
+                                <div class="title">定制化开发</div>
                                 <div class="sub-title">便捷下载，代码全部开源，无加密，且免费可以商用</div>
                             </div>
                         </div>
                         <div class="layui-col-md4">
                             <div class="block">
-                                <div class="image"><img src="https://www.huocms.com/static/home/images/new_index/Advantage_ico02.png"/></div>
-                                <div class="title">开源、免费、可商用</div>
+                                <div class="image"><img class="img" src="https://www.huocms.com/static/home/images/new_index/new01.jpg"/></div>
+                                <div class="title">全面支持与维护</div>
                                 <div class="sub-title">便捷下载，代码全部开源，无加密，且免费可以商用</div>
                             </div>
                         </div>
 
                         <div class="layui-col-md4">
                             <div class="block">
-                                <div class="image"><img src="https://www.huocms.com/static/home/images/new_index/Advantage_ico01.png"/></div>
-                                <div class="title">开源、免费、可商用</div>
-                                <div class="sub-title">便捷下载，代码全部开源，无加密，且免费可以商用</div>
+                                <div class="image"><img class="img" src="https://www.huocms.com/static/home/images/new_index/Advantage_ico01.png"/></div>
+                                <div class="title">优化升级服务</div>
+                                <div class="sub-title">专业的培训课程和咨询服务，助力您的团队快速掌握HonoCMS的最佳实践。</div>
                             </div>
                         </div>
                         <div class="layui-col-md4">
                             <div class="block">
-                                <div class="image"><img src="https://www.huocms.com/static/home/images/new_index/Advantage_ico02.png"/></div>
+                                <div class="image"><img class="img" src="https://www.huocms.com/static/home/images/new_index/Advantage_ico02.png"/></div>
                                 <div class="title">开源、免费、可商用</div>
                                 <div class="sub-title">便捷下载，代码全部开源，无加密，且免费可以商用</div>
                             </div>
--- src/web/view/default/indexView.ts
+++ src/web/view/default/indexView.ts
@@ -2,6 +2,7 @@ import {html,raw} from "hono/html";
 import {Context} from "hono";
 import {homeBannerComponent} from "@/web/view/default/component/homeBannerComponent";
 import {homeAdvantageComponent} from "@/web/view/default/component/homeAdvantageComponent";
+import {homeBusinessComponent} from "@/web/view/default/component/homeBusinessComponent";
 
 export const indexView = (c:Context) =>
     html`
@@ -11,119 +12,13 @@ export const indexView = (c:Context) =>
         <div>
             ${homeAdvantageComponent(c)}
         </div>
-        
-        
-        <div class="layui-container">
-            <div class="layui-row layui-col-space20">
-
-                <!-- 右侧描述 -->
-                <div class="layui-col-md12">
-                    <div class="layui-panel" style="padding: 20px;">
-                        <p>Honocms 是一个致力于为用户提供卓越网站构建体验的开源项目。</p>
-                        <p>无论你是开发者还是普通用户，Honocms 都能为你提供强大的功能和灵活的操作方式。</p>
-                        <button class="layui-btn layui-btn-lg layui-btn-normal">了解更多</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="layui-row layui-col-space20">
-
-                <!-- 右侧描述 -->
-                <div class="layui-col-md12">
-                    <div class="layui-panel" style="padding: 20px;">
-                        <p>Honocms 是一个致力于为用户提供卓越网站构建体验的开源项目。</p>
-                        <p>无论你是开发者还是普通用户，Honocms 都能为你提供强大的功能和灵活的操作方式。</p>
-                        <button class="layui-btn layui-btn-lg layui-btn-normal">了解更多</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="layui-row layui-col-space20">
-
-                <!-- 右侧描述 -->
-                <div class="layui-col-md12">
-                    <div class="layui-panel" style="padding: 20px;">
-                        <p>Honocms 是一个致力于为用户提供卓越网站构建体验的开源项目。</p>
-                        <p>无论你是开发者还是普通用户，Honocms 都能为你提供强大的功能和灵活的操作方式。</p>
-                        <button class="layui-btn layui-btn-lg layui-btn-normal">了解更多</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="layui-row layui-col-space20">
-
-                <!-- 右侧描述 -->
-                <div class="layui-col-md12">
-                    <div class="layui-panel" style="padding: 20px;">
-                        <p>Honocms 是一个致力于为用户提供卓越网站构建体验的开源项目。</p>
-                        <p>无论你是开发者还是普通用户，Honocms 都能为你提供强大的功能和灵活的操作方式。</p>
-                        <button class="layui-btn layui-btn-lg layui-btn-normal">了解更多</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="layui-row layui-col-space20">
-
-                <!-- 右侧描述 -->
-                <div class="layui-col-md12">
-                    <div class="layui-panel" style="padding: 20px;">
-                        <p>Honocms 是一个致力于为用户提供卓越网站构建体验的开源项目。</p>
-                        <p>无论你是开发者还是普通用户，Honocms 都能为你提供强大的功能和灵活的操作方式。</p>
-                        <button class="layui-btn layui-btn-lg layui-btn-normal">了解更多</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="layui-row layui-col-space20">
-
-                <!-- 右侧描述 -->
-                <div class="layui-col-md12">
-                    <div class="layui-panel" style="padding: 20px;">
-                        <p>Honocms 是一个致力于为用户提供卓越网站构建体验的开源项目。</p>
-                        <p>无论你是开发者还是普通用户，Honocms 都能为你提供强大的功能和灵活的操作方式。</p>
-                        <button class="layui-btn layui-btn-lg layui-btn-normal">了解更多</button>
-                    </div>
-                </div>
-            </div>
-
-            <div class="layui-row layui-col-space20">
-
-                <!-- 右侧描述 -->
-                <div class="layui-col-md12">
-                    <div class="layui-panel" style="padding: 20px;">
-                        <p>Honocms 是一个致力于为用户提供卓越网站构建体验的开源项目。</p>
-                        <p>无论你是开发者还是普通用户，Honocms 都能为你提供强大的功能和灵活的操作方式。</p>
-                        <button class="layui-btn layui-btn-lg layui-btn-normal">了解更多</button>
-                    </div>
-                </div>
-            </div>
+        <div>
+            ${homeBusinessComponent(c)}
         </div>
         
-        <div class="layui-container">
-
-            <!-- 功能特点 -->
-            <div class="layui-row layui-col-space20" style="margin-top: 30px;">
-                <h2 style="text-align: center; color: #555;">功能特点</h2>
-                <div class="layui-col-md4">
-                    <div class="layui-panel" style="height: 100%;">
-                        <h3>易用性</h3>
-                        <p>简单直观的界面设计，让你轻松上手。</p>
-                    </div>
-                </div>
-                <div class="layui-col-md4">
-                    <div class="layui-panel" style="height: 100%;">
-                        <h3>可定制性</h3>
-                        <p>丰富的插件和主题选择，满足你的个性化需求。</p>
-                    </div>
-                </div>
-                <div class="layui-col-md4">
-                    <div class="layui-panel" style="height: 100%;">
-                        <h3>社区支持</h3>
-                        <p>活跃的社区支持，帮助你解决遇到的各种问题。</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
+        
+        
         <script>
             layui.use(['table'], function(){
                 let $ = layui.jquery;
