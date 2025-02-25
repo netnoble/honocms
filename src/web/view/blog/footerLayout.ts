34147d4dfcdd6dd0a3dcaee2e4bcbea41af8c7be
cooperding <cooperding@dingtalk.com>
2025-02-25 21:34:04 +0800

    view

--- src/web/view/blog/footerLayout.ts
+++
@@ -1,8 +0,0 @@
-import {html} from "hono/html";
-import {Context} from "hono";
-
-export const footerLayout = (c:Context) => html`
-  <footer>
-    <address>My Address...</address>
-  </footer>
-`
--- src/web/view/blog/headerLayout.ts
+++
@@ -1,9 +0,0 @@
-import {html} from "hono/html";
-import {Context} from "hono";
-
-export const headerLayout = (c: Context ) => html`
-    <header class="honocms-header">
-        
-    </header>
-`
-
